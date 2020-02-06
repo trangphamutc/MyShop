e94cb91b126bc5febbb7fe49edfadc783aa5f7b8
Trangit <trangit986@gmail.com>
2020-02-06 16:12:38 +0700

    Identity AuthenUserApplication

--- MyShop.Data/MovieDbContext.cs
+++ MyShop.Data/MovieDbContext.cs
@@ -4,10 +4,11 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
 using MyShop.Model.Models;
 namespace MyShop.Data
 {
-    public class MovieDbContext: DbContext
+    public class MovieDbContext: IdentityDbContext<ApplicationUser>
     {
         public MovieDbContext() : base("MovieStoreConnection")
         {
@@ -16,7 +17,10 @@ namespace MyShop.Data
         public DbSet<Movie> Movies { set; get; }
         public DbSet<Country> Countries { set; get; }
         public DbSet<Error> Errors { set; get; }
-
+        public static MovieDbContext Create()
+        {
+            return new MovieDbContext();
+        }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
